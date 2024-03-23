afa442b431d85b6e92bdc222f37f5e1f8b3199f3
Zeyu Yuan <zy161@duke.edu>
2024-03-23 17:46:04 -0400

    finished json2wav conversion

--- AudioProcess.py
+++ AudioProcess.py
@@ -8,12 +8,13 @@ def convert_audio(file_name):
 
     # Get the audio data from the JSON
     audio_data = data['content']
-
+    # Get the audio name from the JSON
+    audio_name = data['date']
     # Decode the base64 encoded audio data
     decoded_audio = base64.b64decode(audio_data)
 
-    # Write the decoded audio data to a .wav file
-    with wave.open('output.wav', 'wb') as wav_file:
+    # Write the decoded audio data to a .wav file, use the audio date as the file name
+    with wave.open(f'{audio_name}.wav', 'wb') as wav_file:
         wav_file.setnchannels(1)  # Set the number of channels (1 for mono, 2 for stereo)
         wav_file.setsampwidth(2)  # Set the sample width (2 bytes for 16-bit audio)
         wav_file.setframerate(44100)  # Set the sample rate (44100 Hz is standard for audio)
