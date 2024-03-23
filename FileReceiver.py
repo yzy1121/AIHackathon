089e9f73996aa4a474690cc7e0b30d8eb099638e
Zeyu Yuan <zy161@duke.edu>
2024-03-23 18:03:06 -0400

    changed file upload logic

--- FileReceiver.py
+++ FileReceiver.py
@@ -1,18 +1,37 @@
 from flask import Flask
 import boto3
 from flask import request
+import logging
 from AudioProcess import convert_audio
+from botocore.exceptions import ClientError
+import os
 
 app = Flask(__name__)
+
+
+def upload_file(file_name, bucket, object_name=None):
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
     file = request.get_json()
     file_name = convert_audio(file)
-    s3 = boto3.client('s3')
     bucket_name = 'hackathonrecordings'
 
     # Upload the file to S3
-    s3.put_object(Body=file, Bucket=bucket_name, Key=file_name)
+    upload_file(file_name, bucket_name)
 
     return 'File uploaded successfully'
