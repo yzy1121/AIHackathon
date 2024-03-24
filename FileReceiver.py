c507f84cc378017ce856c7176c49f42c2c5fb130
Zeyu Yuan <zy161@duke.edu>
2024-03-24 10:45:43 -0400

    added app.py

--- FileReceiver.py
+++ FileReceiver.py
@@ -109,4 +109,4 @@ def upload():
         return jsonify({'error': 'Unsupported Media Type'}), 415
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
